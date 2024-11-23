11a8c894e7e7243c8bb118cac51366d68f347c32
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-24 01:07:17 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,5 @@ const repositoryName = 'vue-output-first'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '/',
-  build: {
-    outDir: ''
-  }
+  base: process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '/'
 })
