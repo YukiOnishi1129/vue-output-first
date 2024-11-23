da4cf60aa1f589c8fb7bd12644f230d8f9d8f8b5
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2024-11-24 00:55:23 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   plugins: [vue()],
   base: process.env.NODE_ENV === 'production' ? `/${repositoryName}/` : '/',
   build: {
-    outDir: 'docs'
+    outDir: ''
   }
 })
